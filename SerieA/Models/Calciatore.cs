cacb15f3127ae550f30922ab0f4c75da33d447e3
Emanuele <emanuelecolinto5@gmail.com>
2024-03-22 11:04:39 +0100

    Creazione model Calciatore

---
+++ SerieA/Models/Calciatore.cs
@@ -0,0 +1,15 @@
+﻿namespace SerieA.Models
+{
+    public class Calciatore
+    {
+        public int ID { get; set; }
+        public string Nome {  get; set; }
+        public string Cognome { get; set; }
+        public int NumeroMaglia { get; set; }
+        public Squadra Squadra {  get; set; }
+
+        public Calciatore() { }
+
+
+    }
+}
--- SerieA/obj/Debug/net8.0/SerieA.AssemblyInfo.cs
+++ SerieA/obj/Debug/net8.0/SerieA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SerieA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436ea31ea2933321f8f5a0337c91795216c5e152")]
 [assembly: System.Reflection.AssemblyProductAttribute("SerieA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SerieA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
